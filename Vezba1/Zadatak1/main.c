def7518ed78b25d86da3a4acd2ffce1d720f68b2
pavlovica <pavlovica98@gmail.com>
2021-03-01 20:14:15 +0100

    Initial commit

--- Vezba1/Zadatak1/main.c
+++ Vezba1/Zadatak1/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main(){
-	printf("Hello World!");
+
+	int a = 3;
+	int b = 8;
+	int c;
+
+	if (a > b) {
+		c = 1;
+}	else if (a < b) {
+		c = 0;
+}
+	printf("%f", c);
 	return 0;
 }
